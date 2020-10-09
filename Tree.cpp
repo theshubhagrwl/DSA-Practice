27cefed8139f116fb934e9317c68aced89df5168
Shubh Agrawal <37265683+theshubhagrwl@users.noreply.github.com>
2020-10-09 17:37:09 +0530

    Add files via upload

---
+++ Tree.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+void preOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+    cout << root->data << " ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+void preOrderItr(Node *root)
+{
+    if (root == NULL)
+        return;
+    else
+    {
+        /* code */
+    }
+}
+
+void postOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+    postOrder(root->left);
+    postOrder(root->right);
+    cout << root->data << " ";
+}
+
+void postOrderItr(Node *root)
+{
+    //
+}
+
+void inOrder(Node *root)
+{
+    if (root == NULL)
+        return;
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+void inOrderItr(Node *root)
+{
+    //
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+}
