04526266f2bc08290ad70bfb4fcdcf8320667b18
Shubh Agrawal <37265683+theshubhagrwl@users.noreply.github.com>
2020-10-13 18:02:19 +0530

    Add files via upload

---
+++ BST.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+};
+
+Node *newNode(int data)
+{
+    Node *temp = new Node();
+    temp->data = data;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+void inOrder(Node *root)
+{
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        cout << root->data << " ";
+        inOrder(root->right);
+    }
+}
+
+Node *insert(Node *root, int data)
+{
+    if (root == NULL)
+        return newNode(data);
+    if (data < root->data)
+    {
+        root->left = insert(root->left, data);
+    }
+    else
+    {
+        root->right = insert(root->right, data);
+    }
+    return root;
+}
+
+Node *minValNode(Node *root)
+{
+    Node *temp = root;
+    while (temp && temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp;
+}
+
+Node *deleteNode(Node *root, int data)
+{
+    if (root == NULL)
+        return root;
+
+    if (data < root->data)
+    {
+        root->left = deleteNode(root->left, data);
+    }
+    else if (data > root->data)
+    {
+        root->right = deleteNode(root->right, data);
+    }
+    else
+    {
+        if (root->left == NULL)
+        {
+            Node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL)
+        {
+            Node *temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        Node *temp = minValNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
+int main()
+{
+    Node *root = NULL;
+    root = insert(root, 8);
+    root = insert(root, 3);
+    root = insert(root, 1);
+    root = insert(root, 6);
+    root = insert(root, 7);
+    root = insert(root, 10);
+    root = insert(root, 14);
+    root = insert(root, 4);
+    inOrder(root);
+    cout << endl;
+    root = deleteNode(root, 10);
+    inOrder(root);
+}
