40bfd89949ae3df1291c83621d43b873fe73c41b
Shubh Agrawal <37265683+theshubhagrwl@users.noreply.github.com>
2020-10-21 10:28:59 +0530

    Create TWOVSTEN.cpp

---
+++ CodeChef/TWOVSTEN.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    long long x;
+	    cin >> x;
+	    if(x%10==0)
+	        cout << 0 <<  endl;
+	    else if(x%5==0)
+	        cout << 1 << endl;
+	    else
+	        cout << -1 <<  endl;
+	}
+	return 0;
+}
