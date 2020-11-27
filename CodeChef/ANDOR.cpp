090d93c82d24aff6a3c701520016d896424d5e5c
Shubh Agrawal <37265683+theshubhagrwl@users.noreply.github.com>
2020-11-27 08:58:27 +0530

    Create ANDOR.cpp

---
+++ CodeChef/ANDOR.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		ll x;
+		cin >> x;
+		cout << 0 << " " <<x <<endl;
+	}
+	return 0;
+}
